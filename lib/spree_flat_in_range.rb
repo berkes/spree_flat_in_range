f0c1123f3839d58a274e69192b45bf195de79afe
Bèr Kessels <ber@webschuur.com>
2012-02-19 11:12:25 +0100

    Clean up Engine declaration.

--- lib/spree_flat_in_range.rb
+++ lib/spree_flat_in_range.rb
@@ -4,21 +4,17 @@ module SpreeFlatInRange
   class Engine < Rails::Engine
     engine_name 'spree_flat_in_range'
 
-    config.autoload_paths += %W(#{config.root}/lib)
-
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
     end
 
     def self.activate
-
     end
 
     config.to_prepare &method(:activate).to_proc
 
     initializer "spree.register.calculators" do |app|
-      require 'calculator/flat_in_range'
       app.config.spree.calculators.shipping_methods << Spree::Calculator::FlatInRange
     end
   end
