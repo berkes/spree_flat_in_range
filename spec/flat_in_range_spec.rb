a2fafb96e894c11c385fec3a3ac4189f454ec3ab
Bèr Kessels <ber@webschuur.com>
2012-02-19 11:16:57 +0100

    After changing to BigNum, spec should test against numbers, not strings.

--- spec/flat_in_range_spec.rb
+++ spec/flat_in_range_spec.rb
@@ -19,7 +19,7 @@ describe Spree::Calculator::FlatInRange do
           mock_model(Spree::LineItem, :amount => 10),
           mock_model(Spree::LineItem, :amount => 20)
         ] 
-      calculator.compute(order).should == "10.0"
+      calculator.compute(order).should == 10.0
     end
 
     it "should return zero above the range" do
@@ -27,7 +27,7 @@ describe Spree::Calculator::FlatInRange do
           mock_model(Spree::LineItem, :amount => 10),
           mock_model(Spree::LineItem, :amount => 50)
         ]
-      calculator.compute(order).should == "0.0"
+      calculator.compute(order).should == 0.0
     end
 
     it "should return zero below the range" do
@@ -35,7 +35,7 @@ describe Spree::Calculator::FlatInRange do
           mock_model(Spree::LineItem, :amount => 2),
           mock_model(Spree::LineItem, :amount => 1)
         ]
-      calculator.compute(order).should == "0.0"
+      calculator.compute(order).should == 0.0
     end
 
     it "should include the lower range edge as 'witin the range'" do
@@ -43,14 +43,14 @@ describe Spree::Calculator::FlatInRange do
           mock_model(Spree::LineItem, :amount => 5),
           mock_model(Spree::LineItem, :amount => 5)
         ]
-      calculator.compute(order).should == "10.0"
+      calculator.compute(order).should == 10.0
     end
     it "should include the upper range edge as 'witin the range'" do
       order = mock_model Spree::Order, :line_items => [
           mock_model(Spree::LineItem, :amount => 40),
           mock_model(Spree::LineItem, :amount => 10)
         ]
-      calculator.compute(order).should == "10.0"
+      calculator.compute(order).should == 10.0
     end
   end
 end
